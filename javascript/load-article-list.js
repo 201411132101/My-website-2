d795136fd1f7428d1938ab0cf4bf3b0fd73971ed
bufan <291817591@qq.com>
2018-06-23 18:54:11 +0800

    Update load-article-list.js

--- javascript/load-article-list.js
+++ javascript/load-article-list.js
@@ -28,7 +28,9 @@ function load_list(tags, time) {
 
                 str += "<div class=\"article-list\">" +
                     "<a href=\"?id=" + object.id + "\">" + object.get("title") + "</a>" +
-                    "<p>" + "updatedAt: " + object.updatedAt + " createdAt: " + object.createdAt + "</p>" +
+                    "<p>" + "updatedAt: " + object.updatedAt + 
+                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                    "createdAt: " + object.createdAt + "</p>" +
                     "<p>" + translate(object.get("content")) + "</p>" +
                     "</div>";
             }
