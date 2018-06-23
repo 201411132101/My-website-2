b42b4ec85702e93b82179f1cf745952bca12b593
bufan <291817591@qq.com>
2018-06-23 18:56:34 +0800

    Update load-article.js

--- javascript/load-article.js
+++ javascript/load-article.js
@@ -11,7 +11,8 @@ function load_article(id) {
             // 查询成功，调用get方法获取对应属性的值
 
             var str = "# " + object.get("title") + "\n" +
-                "<p align='center'>" + object.updatedAt + "</p>\n\n" +
+                "<p align='center'>" + "updatedAt: " + object.updatedAt + "</p>" +
+                "<p align='center'>" + "createdAt: " + object.createdAt + "</p>\n\n" +
                 "------\n" +
                 object.get("content");
 
